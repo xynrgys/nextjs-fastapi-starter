d1ef385a7b0d6858dbffc89130b647efa8d3b228
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-14 12:51:45 -0700

    removing local key

--- api/index.py
+++ api/index.py
@@ -7,10 +7,8 @@ import bcrypt
 import os
 
 
-#url: str = os.environ.get("SUPABASE_URL")
-#key: str = os.environ.get("SUPABASE_KEY")
-url= "http://127.0.0.1:54321"
-key= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 app = FastAPI()
