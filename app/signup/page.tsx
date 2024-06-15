8db1b0934e2c65e1bab5057b73d0f4c99ec43ab6
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-15 12:00:29 -0700

    changed to 'next/navigation' from 'next/router'

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,7 +3,7 @@
 'use client'
 
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function SignUpPage() {
   const [name, setName] = useState<string>('');
