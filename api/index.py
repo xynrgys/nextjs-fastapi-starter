dd60d84943a9ca66046c0e22d338e1225b3df22e
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-19 10:46:00 -0700

    fastapi reset

--- api/index.py
+++ api/index.py
@@ -6,53 +6,11 @@ from app.models import LoginRequest, SignupRequest
 import bcrypt
 import os
 
-origins = [
-    "http://localhost:3000",  # Add the origin(s) you want to allow
-]
-
 #url: str = os.environ.get("SUPABASE_URL")
 #key: str = os.environ.get("SUPABASE_KEY")
 
 app = FastAPI()
 #supabase: Client = create_client(url, key)
-
-# Add CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-def user_exists(key: str = "email", value: str = None):
-    user = supabase.from_("users").select("*").eq(key, value).execute()
-    return len(user.data) > 0
-
-@app.post('/api/auth/login')
-def login(request: LoginRequest):
-    response = supabase.auth.sign_in(email=request.email, password=request.password)
-    if response['error']:
-        raise HTTPException(status_code=400, detail=response['error']['message'])
-    return response['data']
-
-@app.post('/api/auth/signup')
-def signup(request: SignupRequest):
-    print("Received signup request")
-    try:
-        if user_exists(value=request.email):
-            raise HTTPException(status_code=400, detail="User with this email already exists")
-
-        hashed_password = bcrypt.hashpw(request.password.encode('utf-8'), bcrypt.gensalt())
-        response = supabase.auth.sign_up(email=request.email, password=hashed_password.decode('utf-8'))
-
-        if response['error']:
-            print(f"Supabase error: {response['error']}")
-            raise HTTPException(status_code=400, detail=response['error']['message'])
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
-
 
 @app.get("/api/python")
 def hello_world():
