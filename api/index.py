d22b8a162d6dc9b1ff535aa9b146ab68fa89d5da
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-19 11:39:18 -0700

    removed bcrypt

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from supabase import create_client, Client
 from app.models import SignupRequest
-import bcrypt
 import os
 
 url: str = os.environ.get("SUPABASE_URL")
@@ -23,8 +22,7 @@ def signup(request: SignupRequest):
     if user_exists(value=request.email):
         raise HTTPException(status_code=400, detail="User with this email already exists")
 
-    hashed_password = bcrypt.hashpw(request.password.encode('utf-8'), bcrypt.gensalt())
-    response = supabase.auth.sign_up(email=request.email, password=hashed_password.decode('utf-8'))
+    response = supabase.auth.sign_up(email=request.email, password=request.password)
 
     if response['error']:
         raise HTTPException(status_code=400, detail=response['error']['message'])
