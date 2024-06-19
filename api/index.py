4abfd281aa974f1f0f43913cabba6a6677b471a2
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-19 12:03:09 -0700

    test if function breaks fastapi

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from supabase import create_client, Client
-from app.models import SignupRequest
+# from app.models import SignupRequest
 import os
 
 url: str = os.environ.get("SUPABASE_URL")
@@ -17,14 +17,14 @@ def user_exists(key: str = "email", value: str = None):
 def hello_world():
     return {"message": "Hello World, supabase connected"}
 
-@app.post('/api/auth/signup', response_model=dict)
-def signup(request: SignupRequest):
-    if user_exists(value=request.email):
-        raise HTTPException(status_code=400, detail="User with this email already exists")
+# @app.post('/api/auth/signup', response_model=dict)
+# def signup(request: SignupRequest):
+#     if user_exists(value=request.email):
+#         raise HTTPException(status_code=400, detail="User with this email already exists")
 
-    response = supabase.auth.sign_up(email=request.email, password=request.password)
+#     response = supabase.auth.sign_up(email=request.email, password=request.password)
 
-    if response['error']:
-        raise HTTPException(status_code=400, detail=response['error']['message'])
+#     if response['error']:
+#         raise HTTPException(status_code=400, detail=response['error']['message'])
 
-    return {"message": "Signup successful"}
+#     return {"message": "Signup successful"}
