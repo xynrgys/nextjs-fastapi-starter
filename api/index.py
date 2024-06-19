15bb1cabb6d846d352a7df109047f622737f7722
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-19 10:49:05 -0700

    full fastapi reset

--- api/index.py
+++ api/index.py
@@ -1,17 +1,7 @@
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.middleware.cors import CORSMiddleware
-from typing import Union
-from supabase import create_client, Client
-from app.models import LoginRequest, SignupRequest
-import bcrypt
-import os
-
-#url: str = os.environ.get("SUPABASE_URL")
-#key: str = os.environ.get("SUPABASE_KEY")
+from fastapi import FastAPI
 
 app = FastAPI()
-#supabase: Client = create_client(url, key)
 
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World, supabase connected"}
+    return {"message": "Hello World"}
