56e309f8a00733c760dbc93bf0db7c3667af373d
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-19 10:58:51 -0700

    add supabase to fastapi

--- api/index.py
+++ api/index.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 
 app = FastAPI()
+supabase: Client = create_client(url, key)
 
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": "Hello World, supabase connected"}
