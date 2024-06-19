b8574391de442f77593668b4888acd52bcb06c33
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-19 12:01:19 -0700

    post test

--- api/app/models.py
+++ api/app/models.py
@@ -1,9 +1,9 @@
-# from pydantic import BaseModel
+from pydantic import BaseModel
 
-# class SignupRequest(BaseModel):
-#     email: str
-#     password: str
+class SignupRequest(BaseModel):
+    email: str
+    password: str
 
-# class LoginRequest(BaseModel):
-#     email: str
-#     password: str
+class LoginRequest(BaseModel):
+    email: str
+    password: str
--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from supabase import create_client, Client
-# from app.models import SignupRequest
+from app.models import SignupRequest
 import os
 
 url: str = os.environ.get("SUPABASE_URL")
@@ -9,22 +9,22 @@ key: str = os.environ.get("SUPABASE_KEY")
 app = FastAPI()
 supabase: Client = create_client(url, key)
 
-# def user_exists(key: str = "email", value: str = None):
-#     user = supabase.from_("users").select("*").eq(key, value).execute()
-#     return len(user.data) > 0
+def user_exists(key: str = "email", value: str = None):
+    user = supabase.from_("users").select("*").eq(key, value).execute()
+    return len(user.data) > 0
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World, supabase connected"}
 
-# @app.post('/api/auth/signup', response_model=dict)
-# def signup(request: SignupRequest):
-#     if user_exists(value=request.email):
-#         raise HTTPException(status_code=400, detail="User with this email already exists")
+@app.post('/api/auth/signup', response_model=dict)
+def signup(request: SignupRequest):
+    if user_exists(value=request.email):
+        raise HTTPException(status_code=400, detail="User with this email already exists")
 
-#     response = supabase.auth.sign_up(email=request.email, password=request.password)
+    response = supabase.auth.sign_up(email=request.email, password=request.password)
 
-#     if response['error']:
-#         raise HTTPException(status_code=400, detail=response['error']['message'])
+    if response['error']:
+        raise HTTPException(status_code=400, detail=response['error']['message'])
 
-#     return {"message": "Signup successful"}
+    return {"message": "Signup successful"}
